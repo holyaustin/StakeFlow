0d7313530540324d3ee9680e2b74781a7fc482b4
holyaustin <holyaustin@yahoo.com>
2024-06-20 21:39:25 +0100

    stakeflow

--- dapp/src/ui/components/Logo.jsx
+++ dapp/src/ui/components/Logo.jsx
@@ -1,5 +1,5 @@
 function Logo() {
-  return <h6 className="text-xl text-white font-extrabold">BWC</h6>;
+  return <h6 className="text-xl text-white font-extrabold">StakeFlow</h6>;
 }
 
 export default Logo;
