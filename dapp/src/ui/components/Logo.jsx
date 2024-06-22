67eaf9628b3e93ab57479ad0ee11a0a96b7d192a
holyaustin <holyaustin@yahoo.com>
2024-06-22 12:22:40 +0100

    dom error commit

--- dapp/src/ui/components/Logo.jsx
+++ dapp/src/ui/components/Logo.jsx
@@ -1,6 +1,6 @@
 import asset from "../../assets/starknet.png";
 function Logo() {
-  return <div class="flex flex-row">
+  return <div className="flex flex-row">
     <img src={asset} className="ml-1 h-8 w-8 mr-5" alt="logo" />
     <h6 className="text-2xl text-white font-extrabold"> StakeFlow  </h6>
   </div>;
